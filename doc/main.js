b15fc9499c687e8c0737cb0cdfd6281f4d3f32cf
Manish Chaudhury <manishchaudhury111222@gmail.com>
2025-12-19 04:29:28 +0530

    Update main.js

--- doc/main.js
+++ doc/main.js
@@ -1,1 +1,12 @@
+function generateQR() {
+  const text = document.getElementById("qrText").value;
+  const qrImage = document.getElementById("qrImage");
 
+  if (text.trim() === "") {
+    alert("Kuch text ya URL likho");
+    return;
+  }
+
+  const qrAPI = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=";
+  qrImage.src = qrAPI + encodeURIComponent(text);
+}
